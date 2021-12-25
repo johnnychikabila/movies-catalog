3c645868940bcbd369b59dcdb5d28e4f86b53a73
ivaan <johnnychikabila@gmail.com>
2021-12-26 00:30:11 +0200

    refactoring with use hooks

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,55 +1,110 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-class Search extends React.Component {
-    state = {
-        search: '',
-        type: 'all',
-    }
+const Search = (props) => {
+  const [search, setSearch] = useState('');
+  const [type, setType] = useState('all');
 
-  handleKeyPress = (event) => { //функція котра отримує івент
-    if (event.key === 'Enter') { // метод визначення клавіші для івенту
-        this.props.searchInputFunc(this.state.search, this.state.type); //передаємо значення інпута зі стейту в параметр функції отриманої з пропсів
-    }
+
+const handleKeyPress = (event) => { //функція котра отримує івент
+  if (event.key === 'Enter') { // метод визначення клавіші для івенту
+      props.searchInputFunc(search, type); //передаємо значення інпута зі стейту в параметр функції отриманої з пропсів
   }
-
-  handleChangeType = (event) => {
-    this.setState(() => ({type: event.target.dataset.type}), () => {
-      this.props.searchInputFunc(this.state.search, this.state.type);
-    })
 }
 
-    render() {
-          const {type} = this.state;
-        return ( <div className="row">
-        <div className="col s12">
-          
-          <div className="input-field">
-            <input 
-            className="validate Placeholder" 
-            placeholder='search' 
-            type="search" 
-            value={this.state.search}
-            onChange={(e) => this.setState({search: e.target.value})}
-            onKeyDown={this.handleKeyPress}
-            />
-            <button className='btn search-btn' onClick={()=> this.props.searchInputFunc(this.state.search, this.state.type)} type="submit">Search</button>
-            <br />
-            <form>
-            <label >
-            <input className='with-gap' type="radio" name="type" value="all" onChange={this.handleChangeType} data-type='all' checked={type === 'all'} /> <span>All</span>
-            </label>
-            <label >
-            <input className='with-gap' type="radio" name="type" value="movie" onChange={this.handleChangeType} data-type='movie' checked={type === 'movie'}/> <span>Movies</span>
-            </label>
-            <label >
-            <input className='with-gap' type="radio" name="type" value="series" onChange={this.handleChangeType} data-type='series' checked={type === 'series'}/> <span>Series</span>
-            </label>
-            </form>
-          </div>
+const handleChangeType = (event) => {
+  setType(event.target.dataset.type);
+  props.searchInputFunc(search, event.target.dataset.type);
+  
+}
+
+      return ( <div className="row">
+      <div className="col s12">
+        
+        <div className="input-field">
+          <input 
+          className="validate Placeholder" 
+          placeholder='search' 
+          type="search" 
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={handleKeyPress}
+          />
+          <button className='btn search-btn' onClick={()=> props.searchInputFunc(search, type)} type="submit">Search</button>
+          <br />
+          <form>
+          <label >
+          <input className='with-gap' type="radio" name="type" value="all" onChange={handleChangeType} data-type='all' checked={type === 'all'} /> <span>All</span>
+          </label>
+          <label >
+          <input className='with-gap' type="radio" name="type" value="movie" onChange={handleChangeType} data-type='movie' checked={type === 'movie'}/> <span>Movies</span>
+          </label>
+          <label >
+          <input className='with-gap' type="radio" name="type" value="series" onChange={handleChangeType} data-type='series' checked={type === 'series'}/> <span>Series</span>
+          </label>
+          </form>
         </div>
       </div>
+    </div>
 
-        )
-    }
+      )
 }
 export {Search}
+
+
+
+
+//old version with class component
+
+// class Search extends React.Component {
+//     state = {
+//         search: '',
+//         type: 'all',
+//     }
+
+//   handleKeyPress = (event) => { //функція котра отримує івент
+//     if (event.key === 'Enter') { // метод визначення клавіші для івенту
+//         this.props.searchInputFunc(this.state.search, this.state.type); //передаємо значення інпута зі стейту в параметр функції отриманої з пропсів
+//     }
+//   }
+
+//   handleChangeType = (event) => {
+//     this.setState(() => ({type: event.target.dataset.type}), () => {
+//       this.props.searchInputFunc(this.state.search, this.state.type);
+//     })
+// }
+
+//     render() {
+//           const {type} = this.state;
+//         return ( <div className="row">
+//         <div className="col s12">
+          
+//           <div className="input-field">
+//             <input 
+//             className="validate Placeholder" 
+//             placeholder='search' 
+//             type="search" 
+//             value={this.state.search}
+//             onChange={(e) => this.setState({search: e.target.value})}
+//             onKeyDown={this.handleKeyPress}
+//             />
+//             <button className='btn search-btn' onClick={()=> this.props.searchInputFunc(this.state.search, this.state.type)} type="submit">Search</button>
+//             <br />
+//             <form>
+//             <label >
+//             <input className='with-gap' type="radio" name="type" value="all" onChange={this.handleChangeType} data-type='all' checked={type === 'all'} /> <span>All</span>
+//             </label>
+//             <label >
+//             <input className='with-gap' type="radio" name="type" value="movie" onChange={this.handleChangeType} data-type='movie' checked={type === 'movie'}/> <span>Movies</span>
+//             </label>
+//             <label >
+//             <input className='with-gap' type="radio" name="type" value="series" onChange={this.handleChangeType} data-type='series' checked={type === 'series'}/> <span>Series</span>
+//             </label>
+//             </form>
+//           </div>
+//         </div>
+//       </div>
+
+//         )
+//     }
+// }
+// export {Search}
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,57 +1,39 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Movies } from "../components/Movies";
 import { Preloader } from '../components/Preloader';
 import { Search } from '../components/Search';
 
 const API_KEY = process.env.REACT_APP_API_KEY; // шлях до ключа API в файлі - .env.local
 
-class Main extends Component {
-    state = {
-       movies: [],
-       loading: true,
-    };
+const Main = () => {
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    componentDidMount() {
-        console.log('mount');
-        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`) //запит для видачі стартової сторінки
-        .then(response => response.json())
-        .then(data => this.setState({movies: data.Search, loading: false}))
-        .catch((err) => {
-            console.log(err);
-            this.setState({loading: false});
-        })
-    }
 
-    // searchInputFunc = (find = 'matrix', type) => {
-    //      // функція з параметром find котрий буде отримувати значення для запиту на сервер
-    //     if (type == 'movie') {fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${find}&type=movie`)
-    //     .then(response => response.json())
-    //     .then(data => this.setState({movies: data.Search, loading: false}))}
-    //     else if (type == 'series') {fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${find}&type=series`)
-    //     .then(response => response.json())
-    //     .then(data => this.setState({movies: data.Search, loading: false}))}
-    //     else {fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${find}`)
-    //     .then(response => response.json())
-    //     .then(data => this.setState({movies: data.Search, loading: false}))}
-    // }
-    
-    //компактніший і лаконічніший варіант
-    searchInputFunc = (find = 'matrix', type = 'all') => {
-        this.setState({loading: true})
+    const searchInputFunc = (find = 'matrix', type = 'all') => {
+        setLoading(true)
        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${find}${type !== 'all' ? `&type=${type}` : ''}`)
        .then(response => response.json())
-       .then(data => this.setState({movies: data.Search, loading: false}))
+       .then(data => {setMovies(data.Search); setLoading(false)})
        .catch((err) => {
         console.log(err);
-        this.setState({loading: false});
+        setLoading(false);
     })
    }
     
+   useEffect(() => {
+    console.log('mount');
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`) //запит для видачі стартової сторінки
+    .then(response => response.json())
+    .then(data => {setMovies(data.Search); setLoading(false)})
+    .catch((err) => {
+        console.log(err);
+        setLoading(false);
+    })
+   },[]);
 
-render() {
-    const {movies, loading} = this.state;
     return ( <main className='content container'>
-        <Search searchInputFunc={this.searchInputFunc} />
+        <Search searchInputFunc={searchInputFunc} />
         {
            loading ? <Preloader /> : <Movies movies={movies }/>
         }
@@ -59,6 +41,66 @@ render() {
          
     )
 }
-
-}
 export {Main}
+
+
+
+
+// old version with class component
+// class Main extends Component {
+//     state = {
+//        movies: [],
+//        loading: true,
+//     };
+
+//     componentDidMount() {
+//         console.log('mount');
+//         fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`) //запит для видачі стартової сторінки
+//         .then(response => response.json())
+//         .then(data => this.setState({movies: data.Search, loading: false}))
+//         .catch((err) => {
+//             console.log(err);
+//             this.setState({loading: false});
+//         })
+//     }
+
+//     // searchInputFunc = (find = 'matrix', type) => {
+//     //      // функція з параметром find котрий буде отримувати значення для запиту на сервер
+//     //     if (type == 'movie') {fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${find}&type=movie`)
+//     //     .then(response => response.json())
+//     //     .then(data => this.setState({movies: data.Search, loading: false}))}
+//     //     else if (type == 'series') {fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${find}&type=series`)
+//     //     .then(response => response.json())
+//     //     .then(data => this.setState({movies: data.Search, loading: false}))}
+//     //     else {fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${find}`)
+//     //     .then(response => response.json())
+//     //     .then(data => this.setState({movies: data.Search, loading: false}))}
+//     // }
+    
+//     //компактніший і лаконічніший варіант
+//     searchInputFunc = (find = 'matrix', type = 'all') => {
+//         this.setState({loading: true})
+//        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${find}${type !== 'all' ? `&type=${type}` : ''}`)
+//        .then(response => response.json())
+//        .then(data => this.setState({movies: data.Search, loading: false}))
+//        .catch((err) => {
+//         console.log(err);
+//         this.setState({loading: false});
+//     })
+//    }
+    
+
+// render() {
+//     const {movies, loading} = this.state;
+//     return ( <main className='content container'>
+//         <Search searchInputFunc={this.searchInputFunc} />
+//         {
+//            loading ? <Preloader /> : <Movies movies={movies }/>
+//         }
+//     </main>
+         
+//     )
+// }
+
+// }
+// export {Main}
